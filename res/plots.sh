71ba753be9a8e028497abb4d430f0bae2a8e5263
slawomir <sgicala98@gmail.com>
2019-06-01 16:48:05 +0200

    results are now written into files, added makefile plots making script

---
+++ res/plots.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+gnuplot 'sin_plot'
+gnuplot 'sin_fft_plot'
--- src/sinus_sum.f90
+++ src/sinus_sum.f90
@@ -1,38 +1,32 @@
-program test
+program main
 
     use, intrinsic :: iso_c_binding
+    implicit none
     include 'fftw3.f03'
     real(kind=16), parameter :: PI_16 = 4*atan(1.0_16)
     type(C_PTR) :: plan
-    complex(C_DOUBLE_COMPLEX), dimension(1000) :: in, out
+    real(C_DOUBLE), dimension(10000) :: in
+    complex(C_DOUBLE_COMPLEX), dimension(10000) :: out
+    integer :: i
+    real(kind=16) :: t
 
 
+    open(1, file="../res/sinus_sum_time_amplitude.txt", action='WRITE')
+    open(2, file="../res/sinus_sum_frequency_amplitude.txt", action='WRITE')
 
-    do i=1,1000
-        t = i/1000.0
+    do i=1,10000
+        t = i/10000.0
         in(i) = sin(2*PI_16*t*200)+2*sin(2*PI_16*t*400)
-        write(*,*) in(i)
+        write(1,*) t, " ", in(i)
     end do
 
+    plan = fftw_plan_dft_r2c_1d(10000, in, out, FFTW_FORWARD + FFTW_ESTIMATE)
+    call fftw_execute_dft_r2c(plan, in, out)
 
-
-    plan = fftw_plan_dft_1d(1000, in, out, FFTW_FORWARD, FFTW_ESTIMATE)
-    call fftw_execute_dft(plan, in, out)
-
-
-    write(*,*) out
-
-
-
-    do i=1,1000
-        write(*,*) abs(out(i))
+    do i=1,size(out)
+        write(2,*) i, " ", abs(out(i))
     end do
-
-
 
     call fftw_destroy_plan(plan)
 
-
-
-
-end program test
+end program main
